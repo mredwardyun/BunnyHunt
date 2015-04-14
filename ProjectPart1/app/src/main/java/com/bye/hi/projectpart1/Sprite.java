a11f834b2e00b63cf24c43bdef5613e7ad9a83cf
Zeke <zeke.tan@vanderbilt.edu>
2015-04-13 21:35:32 -0500

    checkpoint2

---
+++ ProjectPart1/app/src/main/java/com/bye/hi/projectpart1/Sprite.java
@@ -0,0 +1,25 @@
+package com.bye.hi.projectpart1;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Zeke on 13/4/15.
+ */
+public class Sprite {
+
+    private String name;
+    private int moveDirection;
+    private boolean isMoving;
+    private Bitmap sprite;
+
+
+    public void move(int xPos, int yPos) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
